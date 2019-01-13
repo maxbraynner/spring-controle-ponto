37c4ba7dafa16822a97f4d8367a84711f36fbfb5
Max Braynner <max.braynner@gmail.com>
2019-01-13 19:12:46 -0300

    Adição de enums

---
+++ src/main/java/com/controleponto/api/enums/TipoLancamentoEnum.java
@@ -0,0 +1,12 @@
+package com.controleponto.api.enums;
+
+public enum TipoLancamentoEnum {
+
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA;
+	
+}
