8546f72968ae52676c631790c9b3dd3e9f98e93b
Max Braynner <max.braynner@gmail.com>
2019-01-15 22:54:16 -0300

    Ajuste dos testes

--- src/test/java/com/controleponto/api/ControlePontoApplicationTests.java
+++ src/test/java/com/controleponto/api/ControlePontoApplicationTests.java
@@ -3,10 +3,12 @@ package com.controleponto.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Profile("test")
 public class ControlePontoApplicationTests {
 
 	@Test
